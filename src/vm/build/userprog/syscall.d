377f7b1120fce819dd1936149acd469581850b25
Scott Stephens <stevo4932@utexas.edu>
2014-10-30 18:40:24 -0500

    made vm

---
+++ src/vm/build/userprog/syscall.d
@@ -0,0 +1,9 @@
+userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
+  ../../filesys/file.h ../../filesys/off_t.h ../../lib/stdint.h \
+  ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+  ../../lib/stdbool.h ../../lib/stddef.h ../../lib/kernel/stdio.h \
+  ../../lib/syscall-nr.h ../../threads/interrupt.h ../../threads/thread.h \
+  ../../lib/kernel/list.h ../../threads/synch.h ../../devices/shutdown.h \
+  ../../threads/vaddr.h ../../threads/loader.h ../../userprog/pagedir.h \
+  ../../userprog/process.h ../../threads/palloc.h ../../threads/malloc.h \
+  ../../filesys/filesys.h ../../devices/input.h ../../lib/string.h
